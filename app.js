a6af907103d5f4c880f6d410f65990b540dd705f
Costa Michailidis <costa@innovationbound.com>
2019-07-18 19:55:23 -0400

    working

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-!(function () {
-  "use strict"
-  window.addEventListener('load', function (event) {
 
-    var board = document.getElementById('board')
-    var rows = 6
-    var columns = 7
+var board = document.getElementById('board')
+var rows = 6
+var columns = 7
 
-    board.style.setProperty('grid-template-columns', `repeat(${columns}, 1fr)`)
-    board.style.setProperty('grid-template-rows', `repeat(${rows}, 1fr)`)
+board.style.setProperty('grid-template-columns', `repeat(${columns}, 1fr)`)
+board.style.setProperty('grid-template-rows', `repeat(${rows}, 1fr)`)
 
-    for (var i = 1; i <= rows * columns; i += 1) {
-      var space = document.createElement('template')
-      space.innerHTML = `<span class="space" id="space-${i}">${i}</span>`
-      var clone = document.importNode(space.content, true)
-      board.append(clone)
-    }
-
-  })
-}());
+for (var i = 1; i <= rows * columns; i += 1) {
+  var space = document.createElement('template')
+  space.innerHTML = `<span class="space" id="space-${i}">${i}</span>`
+  var clone = document.importNode(space.content, true)
+  board.append(clone)
+}
 
