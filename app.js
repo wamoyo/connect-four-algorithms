2c1f20c37dac1245d53d2d35c5f6e907ac7562f8
Costa Michailidis <costa@innovationbound.com>
2019-07-19 17:55:40 -0400

    css adjustments

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ board.style.setProperty('grid-template-rows', `repeat(${rows}, 1fr)`)
 for (var i = 1; i <= rows * columns; i += 1) {
   var space = document.createElement('template')
   space.innerHTML = `<span class="space" id="space-${i}">${i}</span>`
-  if (i === 31) space.innerHTML = `<span class="space available" id="space-${i}">${i}</span>` 
-  if (i === 32) space.innerHTML = `<span class="space available" id="space-${i}">${i}</span>` 
-  if (i === 36) space.innerHTML = `<span class="space available" id="space-${i}">${i}</span>` 
-  if (i === 37) space.innerHTML = `<span class="space available" id="space-${i}">${i}</span>` 
-  if (i === 40) space.innerHTML = `<span class="space available" id="space-${i}">${i}</span>` 
-  if (i === 41) space.innerHTML = `<span class="space available" id="space-${i}">${i}</span>` 
-  if (i === 42) space.innerHTML = `<span class="space available" id="space-${i}">${i}</span>` 
-  if (i === 38) space.innerHTML = `<span class="space red" id="space-${i}">${i}</span>` 
-  if (i === 39) space.innerHTML = `<span class="space blue" id="space-${i}">${i}</span>` 
+  if (i === 31) space.innerHTML = `<span class="space available" id="space-${i}">${i}</span>`
+  if (i === 32) space.innerHTML = `<span class="space available" id="space-${i}">${i}</span>`
+  if (i === 36) space.innerHTML = `<span class="space available" id="space-${i}">${i}</span>`
+  if (i === 37) space.innerHTML = `<span class="space available" id="space-${i}">${i}</span>`
+  if (i === 40) space.innerHTML = `<span class="space available" id="space-${i}">${i}</span>`
+  if (i === 41) space.innerHTML = `<span class="space available" id="space-${i}">${i}</span>`
+  if (i === 42) space.innerHTML = `<span class="space available" id="space-${i}">${i}</span>`
+  if (i === 38) space.innerHTML = `<span class="space red" id="space-${i}">${i}</span>`
+  if (i === 39) space.innerHTML = `<span class="space blue" id="space-${i}">${i}</span>`
   var clone = document.importNode(space.content, true)
   board.append(clone)
 }
